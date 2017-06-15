0424e2e1955f0cc79c8ef613bbcf582b017a3bca
Shane <shane.cawser@relexsolutions.com>
2017-06-15 13:34:13 +0100

    Implemented new grid generation with random ships.

--- src/Battleships.java
+++ src/Battleships.java
@@ -9,11 +9,13 @@
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.*;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import jdk.nashorn.internal.ir.debug.JSONWriter;
 import org.json.simple.parser.ParseException;
 import org.json.simple.parser.JSONParser;
 
@@ -43,19 +45,83 @@ public class Battleships {
 
     static class StartGameHandler extends HandlerWithStringResponse {
         @Override
-        protected String getResponse() {
-            return "{ grid:\n" +
-                "   [ [ '1', ' ', '2', '2', '2', '2', ' ', ' ', ' ', ' ' ],\n" +
-                "     [ '1', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],\n" +
-                "     [ '1', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],\n" +
-                "     [ '1', ' ', '3', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],\n" +
-                "     [ '1', ' ', '3', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],\n" +
-                "     [ ' ', ' ', '3', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],\n" +
-                "     [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],\n" +
-                "     [ ' ', ' ', ' ', ' ', '4', '4', '4', ' ', ' ', ' ' ],\n" +
-                "     [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '5' ],\n" +
-                "     [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '5' ] ]\n" +
-                "}\n";
+        protected String getResponse()
+        {
+            return getNewGrid();
+        }
+    }
+
+    private static String getNewGrid()
+    {
+        String[][] grid = new String[10][10];
+        List<Ship> ships = Arrays.asList(new Ship("Carrier", 5), new Ship("Battleship", 4), new Ship("Cruiser", 3), new Ship("Submarine", 3), new Ship("Destroyer", 2));
+
+        while (ships.size()> 0)
+        {
+            Ship ship = ships.get(0);
+            int positionX = 0 + (int)(Math.random() * 9);
+            int positionY = 0 + (int)(Math.random() * 9);
+            Boolean horizontal = (0 + (int)(Math.random() * 100)) % 2 == 1;
+
+            if (grid[positionX][positionY] != null || !CheckShipCanFit(grid, ship.Size, positionX, positionY, horizontal)) continue;
+            grid[positionX][positionY] = ship.Name;
+            if (horizontal)
+            {
+                for (int i = 0; i < ship.Size; i++) grid[positionX+i][positionY] = ship.Name;
+            }
+            else
+            {
+                for (int i = 0; i < ship.Size; i++) grid[positionX][positionY + i] = ship.Name;
+            }
+            ships.remove(0);
+        }
+        return grid.toString();
+    }
+
+    private static Boolean CheckShipCanFit(String[][] grid, int shipSize, int positionX, int positionY, Boolean horizontal)
+    {
+        //check to see if there are enough spots to fit ships in
+        Boolean cantFit = horizontal ? positionX + shipSize >= 10 : positionY + shipSize >= 10;
+        if (cantFit) return false;
+
+        //check to see if a shipB already placed in a spot that we need to occupy
+        if (horizontal)
+        {
+            for (int i = positionX; i < shipSize; i++)
+            {
+                if (grid[i][positionY] != null) return false;
+            }
+        }
+        else
+        {
+            for (int i = positionY; i < shipSize; i++)
+            {
+                if (grid[positionX][i] != null) return false;
+            }
+        }
+        return true;
+    }
+
+    private static class Ship
+    {
+        public String Name;
+        public int Size;
+
+        /// <summary>Initializes a new instance of the <see cref="T:System.Object" /> class.</summary>
+        public Ship(String name, int size)
+        {
+            Name = name;
+            Size = size;
+        }
+
+        public String GetName()
+        {
+            return Name;
+        }
+
+        public int GetSize()
+        {
+            return Size;
         }
     }
 
