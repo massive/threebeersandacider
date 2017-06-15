f7ce5b03be5df46550b06018bd51691c0a42093c
Antti Kauppinen <antti.kauppinen@relex.fi>
2017-06-15 15:33:59 +0300

    Implemented random shooting.

--- src/Battleships.java
+++ src/Battleships.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.Random;
 
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
@@ -22,6 +23,7 @@ import org.json.simple.parser.ParseException;
 public class Battleships {
 
     public static void main(String[] args) throws Exception {
+        initializeGrid();
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         System.out.println("Running at " + server.getAddress());
 
@@ -67,6 +69,7 @@ public class Battleships {
     static class StartGameHandler extends HandlerWithStringResponse {
         @Override
         protected String getResponse(JSONObject params) {
+            initializeGrid();
             return "{ grid:\n" +
                 "   [ [ '1', ' ', '2', '2', '2', '2', ' ', ' ', ' ', ' ' ],\n" +
                 "     [ '1', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],\n" +
@@ -87,9 +90,19 @@ public class Battleships {
         @Override
         protected String getResponse(JSONObject params) {
             System.out.println("got " + params.get("name"));
+            int[] target = null;
+            while (target == null) {
+                System.out.println("Calculating target...");
+                try {
+                    target = calculateTarget();
+                } catch (Exception e) {
+                    System.err.println("Error: " + e + ". Trying again...");
+                }
+            }
+            System.out.println("Target: " + target[0] + ", " + target[1]);
             return "{\n" +
-                "  x: 6,\n" +
-                "  y: 2\n" +
+                "  x: " + target[0] + ",\n" +
+                "  y: " + target[1] + "\n" +
                 "}\n";
         }
     }
@@ -101,5 +114,46 @@ public class Battleships {
                 "  \"message\" : \"AAAGH!\"\n" +
                 "}\n";
         }
+    }
+
+    private static final int SIZE_X = 10;
+    private static final int SIZE_Y = 10;
+
+    private enum State {
+        UNKNOWN, SHIP, NO_SHIP
+    };
+
+    private static State[][] grid = new State[SIZE_X][SIZE_Y];
+
+    private static Random random = new Random();
+
+    private static void initializeGrid() {
+        for (int y = 0; y < SIZE_Y; y++) {
+            for (int x = 0; x < SIZE_X; x++) {
+                grid[x][y] = State.UNKNOWN;
+            }
+        }
+    }
+
+    private static int[] calculateTarget() {
+        int[] randomTarget;
+        while (true) {
+            randomTarget = getRandomTarget();
+            if (getState(randomTarget) == State.UNKNOWN) break;
+        }
+        setState(randomTarget, State.NO_SHIP);
+        return randomTarget;
+    }
+
+    private static State getState(int[] target) {
+        return grid[target[0]][target[1]];
+    }
+
+    private static void setState(int[] target, State setTo) {
+        grid[target[0]][target[1]] = setTo;
+    }
+
+    private static int[] getRandomTarget() {
+        return new int[]{Math.abs(random.nextInt()) % SIZE_X, Math.abs(random.nextInt()) % SIZE_Y};
     }
 }
